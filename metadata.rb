ef5c9568a0a9bb562a453aa403dd8a9476706951
Richardson Lima <contatorichardsonlima@gmail.com>
2016-05-29 12:17:17 -0300

    first commit

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
-name             'LAMP'
+name             'lamp'
 maintainer       'RichardsonLima'
 maintainer_email 'contatorichardsonlima@gmail.com'
 license          'All rights reserved'
-description      'Installs/Configures LAMP'
+description      'Installs/Configures lamp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
